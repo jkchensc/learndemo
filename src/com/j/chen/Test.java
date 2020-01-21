6b9d79139ee0b30e8d8ca8ec17a41b3ad85ee219
TECH\shanchang.chen <shanchang.chen@gdc-tech.com>
2020-01-21 11:33:38 +0800

    init demoproject

---
+++ src/com/j/chen/Test.java
@@ -0,0 +1,12 @@
+package com.j.chen;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		String str = "Hello Genius!  ";
+
+		String str3 = str.replaceAll(" ","");
+		System.out.println(str3);
+	}
+
+}
